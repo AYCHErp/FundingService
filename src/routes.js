8bc7bbb41dbb9137571b530e5112e257dccac8f9
nsward <wardnick@protonmail.com>
2019-09-09 20:53:36 -0600

    Update funding route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 "use strict"
 
-// TODO: this is just terrible
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./config/migrationsConfig.json", "utf8"));
+const addresses = config["migration_" + process.env.MIGRATION];
 
 const BChainHandler = require("./BChainHandler.js");
 const ethers = require("ethers");
@@ -8,24 +10,33 @@ const Token = require("../artifacts/DisberseToken.json").abi;
 const ExchangeLogger = require("../artifacts/ExchangeLogger.json").abi;
 
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-const token = new ethers.Contract(process.env.TOKEN_ADDR, Token, provider);
-const exchangeLogger = new ethers.Contract(process.env.EXCHANGE_LOGGER_ADDR, ExchangeLogger, provider);
-const bChainHandler = new BChainHandler(provider, token, exchangeLogger);
+const token = new ethers.Contract(addresses.tokenAddr, Token, provider);
+const tokens = [{symbol: "EUR", contract: token}];
+const exchangeLogger = new ethers.Contract(addresses.exchangeLoggerAddr, ExchangeLogger, provider);
+const bChainHandler = new BChainHandler(provider, exchangeLogger, tokens);
 
 const BIN_NAME = "0x" + Buffer.from("121-project").toString("hex")
 const bin = ethers.utils.keccak256(BIN_NAME);
 
 const routes = [];
 
+console.log(`\n\nSeeded user address: ${addresses.userAddr}`);
+
 routes.push({
-  method: 'GET',
-  path: '/d121p/{address}/burned_funds/{fromBlock}',
+  method: "GET",
+  path: "/funding/{address}/{since_timestamp?}",
   handler: async (request, h) => {
-    await bChainHandler.init();
     try {
-      return await bChainHandler.getBurnedFunds(bin)
+      console.log('request:', request.params);
+      const fromTs = request.params.since_timestamp ? request.params.since_timestamp : 0
+      return await bChainHandler.getBurnedFunds(
+        Number(fromTs),
+        request.params.address,
+        bin
+      );
     } catch(err) {
       console.log(err)
+      throw(err)
     }
   }
 });
